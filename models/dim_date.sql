ef4bd786f1db951dfa533443a12519540e9c7442
moonlight8905@outlook.com <moonlight8905@outlook.com>
2026-08-24 06:53:20 +0000

    dim_date model

---
+++ models/dim_date.sql
@@ -0,0 +1,28 @@
+WITH CTE AS(
+    
+    select
+     TO_TIMESTAMP(STARTED_AT) AS started_at,
+     DATE(TO_TIMESTAMP(STARTED_AT)) AS date_started_at,
+     HOUR(TO_TIMESTAMP(STARTED_AT)) AS hour_started_at,
+     DAYNAME(TO_TIMESTAMP(STARTED_AT)) AS day_started_at,
+     CASE
+        WHEN  DAYNAME(TO_TIMESTAMP(STARTED_AT)) IN('Sat','Sun')
+        THEN 'Weekend'
+        ELSE 'Business'
+    END AS day_type,
+    CASE 
+        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (12,1,2) THEN 'Winter'
+        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (3,4,5) THEN 'Spring'
+        WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) IN (6,7,8) THEN 'Summer'
+        ELSE 'Autumn'
+    END AS season
+
+    from
+    {{ source('demo', 'bike') }}
+    where STARTED_AT != 'started_at'
+)
+
+
+select
+*
+from CTE
